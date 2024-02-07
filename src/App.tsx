3b62411e6b9dbac96248c3f94cd12fda18520bf5
hongyeojung <yeojung@uxstory.co.kr>
2024-02-07 14:55:01 +0900

    eslint,prettier 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from 'page/Home';
+import About from 'page/About';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <a href='/'>home</a>
+            <a href='/about'>about</a>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
