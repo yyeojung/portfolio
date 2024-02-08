59017b0c6e2e9a56ff99ae1ba6bd311cdc9be551
hongyeojung <yeojung@uxstory.co.kr>
2024-02-08 11:58:27 +0900

    esint, prettierrc 수정..

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import Home from 'page/Home';
 import About from 'page/About';
 
 function App() {
-    return (
-        <div className="App">
-            <a href='/'>home</a>
-            <a href='/about'>about</a>
-            <Routes>
-                <Route path='/' element={<Home/>}/>
-                <Route path='/about' element={<About/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <a href='/'>호우</a>
+      <a href='/about'>느리군</a>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
